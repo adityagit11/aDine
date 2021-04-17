db0cc4cc784dc102f8a39af7a93f02e440629977
Aditya Git <adityasinghwolvieait@gmail.com>
2021-04-18 03:46:23 +0530

    Initial Commit

---
+++ aDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  aDine
+//
+//  Created by Aditya Singh on 18/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ aDine/aDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  aDineApp.swift
+//  aDine
+//
+//  Created by Aditya Singh on 18/04/21.
+//
+
+import SwiftUI
+
+@main
+struct aDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
